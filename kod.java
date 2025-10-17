04816f24e1728377e5cf004b91abf75ebb21595c
Becsi1980 <kezdokartya@gmail.com>
2025-10-17 13:08:32 +0200

    Update kod.java

--- kod.java
+++ kod.java
@@ -1,43 +1,104 @@
-let x = 2
-let y = 2
+let rounds = 5
+let currentRound = 0
+let signalShown = false
+let startTime = 0
 
-radio.setGroup(1)
+let playerATime = 0
+let playerBTime = 0
 
+let playerAScore = 0
+let playerBScore = 0
+
+function showReady() {
+    basic.clearScreen()
+    basic.showString("R" + (currentRound + 1))
+    basic.clearScreen()
+}
+
+function showSignal() {
+    basic.showIcon(IconNames.Square)
+    signalShown = true
+    startTime = input.runningTime()
+}
+
+function finishGame() {
+    basic.clearScreen()
+    basic.showString("VEGE")
+
+    // Pontszámok kiírása
+    basic.showString("A:")
+    basic.showNumber(playerAScore)
+    basic.showString("T:")
+    basic.showNumber(playerATime)
+
+    basic.showString("B:")
+    basic.showNumber(playerBScore)
+    basic.showString("T:")
+    basic.showNumber(playerBTime)
+
+    // Győztes kiírása
+    if (playerAScore > playerBScore) {
+        basic.showString("A NYERT")
+    } else if (playerBScore > playerAScore) {
+        basic.showString("B NYERT")
+    } else {
+        basic.showString("DONTETLEN")
+    }
+
+    basic.pause(5000)
+    control.reset()
+}
+
+// Gomb A
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
+    if (!signalShown) {
+        playerATime += 1000 // Büntetés: +1000 ms
+    } else {
+        let reaction = input.runningTime() - startTime
+        playerATime += reaction
+        playerAScore++
+        signalShown = false
+        nextRound()
+    }
 })
 
+// Gomb B
 input.onButtonPressed(Button.B, function () {
-    radio.sendString("Hello")
-    basic.showIcon(IconNames.Heart)
-})
-
-input.onGesture(Gesture.Shake, function () {
-    x = 2
-    y = 2
-    basic.clearScreen()
-    led.plot(x, y)
-})
-
-basic.forever(function () {
-    basic.clearScreen()
-
-    // Mozgás gyorsulásmérővel
-    let ax = input.acceleration(Dimension.X)
-    let ay = input.acceleration(Dimension.Y)
-
-    if (ax > 200) {
-        x = Math.min(4, x + 1)
-    } else if (ax < -200) {
-        x = Math.max(0, x - 1)
+    if (!signalShown) {
+        playerBTime += 1000 // Büntetés: +1000 ms
+    } else {
+        let reaction = input.runningTime() - startTime
+        playerBTime += reaction
+        playerBScore++
+        signalShown = false
+        nextRound()
     }
-
-    if (ay > 200) {
-        y = Math.min(4, y + 1)
-    } else if (ay < -200) {
-        y = Math.max(0, y - 1)
-    }
-
-    led.plot(x, y)
-    basic.pause(200)
 })
+
+function nextRound() {
+    currentRound++
+    if (currentRound >= rounds) {
+        finishGame()
+    } else {
+        playRound()
+    }
+}
+
+function playRound() {
+    showReady()
+    signalShown = false
+    // Egyre gyorsuló játékmenet
+    let minWait = Math.max(1000 - currentRound * 150, 200)
+    basic.pause(randint(minWait, minWait + 1000))
+    showSignal()
+}
+
+// Játék indítása
+currentRound = 0
+playerAScore = 0
+playerBScore = 0
+playerATime = 0
+playerBTime = 0
+
+basic.showString("START")
+playRound()
